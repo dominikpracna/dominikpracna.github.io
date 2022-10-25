bd3e99cb8b6bd28d3188c5344b5182077c2158a1
Dominik Pracna <dominikpracna@gmail.com>
2022-10-25 15:26:26 +0200

    gg

--- arkanoid-ts-startHere/src/sprites/ball.ts
+++ arkanoid-ts-startHere/src/sprites/ball.ts
@@ -1,4 +1,4 @@
-import { Vector } from "~/types";
+import { Vector } from "../types";
 
 
 export class Ball {
--- arkanoid-ts-startHere/src/sprites/paddle.ts
+++ arkanoid-ts-startHere/src/sprites/paddle.ts
@@ -1,4 +1,4 @@
-import { Vector } from "~/types";
+import { Vector } from "../types";
 
 export class Paddle {
     private paddleImage: HTMLImageElement = new Image()
